a69d09f73dee641afa6eeddba5f0f4f5217f8c94
DiegoPereira-Dev <diegoconstancioribeiro@gmail.com>
2024-05-31 23:05:30 -0300

    update responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Animação do nome */
 
 function ativaLetra(elemento) {
     return new Promise(resolve => {
@@ -12,22 +13,37 @@ function ativaLetra(elemento) {
     });
 }
 
-document.addEventListener("DOMContentLoaded", async function() {
+document.addEventListener("DOMContentLoaded", async function () {
     const nomeElement = document.querySelector(".nome");
     const sobrenomeElement = document.querySelector('.pereira');
-    const complemento = document.querySelector('.animation_2'); 
-    const button = document.querySelector('.btn-topo-do-site'); 
+    const complemento = document.querySelector('.animation_2');
+    const button = document.querySelector('.btn-topo-do-site');
 
     await ativaLetra(nomeElement);
     sobrenomeElement.style.visibility = 'visible'; // Torna a palavra "Pereira" visível
     await ativaLetra(sobrenomeElement);
     complemento.style.visibility = 'visible';
-    await ativaLetra(complemento); 
+    await ativaLetra(complemento);
     button.style.visibility = 'visible';
 
 
     console.log("Nome completo concluído.");
 });
 
+/* Menu mobile */
 
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
